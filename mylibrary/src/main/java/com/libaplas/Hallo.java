0e17383c2672c25a8d545673e982e8f41390362d
RifkyLana <rifkylana2016@gmail.com>
2021-11-30 06:15:09 +0800

    initial

--- mylibrary/src/main/java/com/libaplas/Hallo.java
+++ mylibrary/src/main/java/com/libaplas/Hallo.java
@@ -1,4 +1,10 @@
 package com.libaplas;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class Hallo {
+    public static void s(Context c, String message){
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
 }
